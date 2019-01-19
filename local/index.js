4a8082575467a1f45e200186b858f93978ee4afa
Bogdan Alexandru Militaru <boobo94@192-168-0-114.rdsnet.ro>
2019-01-19 15:58:14 +0200

    Display correct port which local server runs

--- local/index.js
+++ local/index.js
@@ -3,8 +3,6 @@ import bodyParser from "body-parser";
 
 import { handler } from "../lambda/custom";
 
-const port = 3000
-
 function CreateHandler(handler) {
     return (req, res) => {
         handler(req.body, null, (err, result) => {
@@ -18,7 +16,8 @@ function CreateHandler(handler) {
 
 // create server
 const server = express();
-server.listen(process.env.port || process.env.PORT || port, function () {
+const port = process.env.port || process.env.PORT || 3000
+server.listen(port, function () {
     console.log(`listening on localhost:${port}`);
 });
 
