34fc175e38711987bc99672a0840405895d87fec
Bogdan Alexandru Militaru <boobo94@Bogdans-MacBook-Pro.local>
2019-01-19 20:42:39 +0200

    Add new file to keep the constants related to pomodoro

---
+++ lambda/custom/intents/pomodoro/constants.js
@@ -0,0 +1,18 @@
+// session duration
+export const sessionDurationM = 25
+export const sessionDurationS = 1500
+
+// states
+
+export const states = {
+    new: "new",
+    session1: "session1",
+    break1: "break1",
+    session2: "session2",
+    break2: "break2",
+    session3: "session3",
+    break3: "break3",
+    session4: "session4",
+    break4: "break4",
+}
+
