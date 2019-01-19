7b0035d6d9dab5c0b4b5f6dbcb6953d6e9016392
Bogdan Alexandru Militaru <boobo94@Bogdans-MacBook-Pro.local>
2019-01-19 17:40:05 +0200

    Add all built in handlers
    - custom
    - fallback
    - stop

--- lambda/custom/locales/en.js
+++ lambda/custom/locales/en.js
@@ -3,5 +3,9 @@ export default {
         "WELCOME": "Welcome at Pomodoro",
         "UNKNOWN_ERROR": "Unknown error",
         "HELP": "Help message",
+        "STOP": "You stopped any activity. If you want to start a new session please say 'New timer'.",
+        "INCOMPREHENSIBLE": "Sorry, but I don't understand. Can you repeat, please?",
+        "INCOMPREHENSIBLE_REPROMT": "Sorry, I don't understand it. Can you reformulate?",
+        "GOODBYE": "Goodbye buddy, hear you soon!"
     }
 }
--- lambda/custom/locales/es.js
+++ lambda/custom/locales/es.js
@@ -3,5 +3,9 @@ export default {
         "WELCOME": "Bienvenido a Pomodoro",
         "UNKNOWN_ERROR": "Error desconocido",
         "HELP": "Mensaje de ayuda",
+        "STOP": "Paraste cualquier actividad. Si desea iniciar una nueva sesión, diga 'Nuevo temporizador'.",
+        "INCOMPREHENSIBLE": "Lo siento, pero no entiendo. ¿Puede usted repetir por favor?",
+        "INCOMPREHENSIBLE_REPROMT": "Lo siento, no lo entiendo. ¿Se puede reformular?",
+        "GOODBYE": "Adiós amigo, te escucho pronto!",
     }
 }
